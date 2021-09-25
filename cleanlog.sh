8f83870003f97b5d19d3ddf928a6442eab11be1d
Simoneva <33739086+Simoneva@users.noreply.github.com>
2021-09-25 16:02:39 +0800

    Update cleanlog.sh

--- cleanlog.sh
+++ cleanlog.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-rm /var/log/alternatives.log.* -f
-rm /var/log/auth.log.* -f
-rm /var/log/debug.* -f
-rm /var/log/daemon.log.* -f
-rm /var/log/dpkg.log.* -f
-rm /var/log/kern.log.* -f
-rm /var/log/messages.* -f
-rm /var/log/syslog.* -f
-rm /var/log/user.log.* -f
+ls | egrep 'auth.log..*|debug..*|daemon.log..*|dpkg.log..*|kern.log..*|messages..*|syslog..*|user.log..*' | xargs rm
 for i in `find /var/log -name "*.log"`; do cat /dev/null >$i; done
