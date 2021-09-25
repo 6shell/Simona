5ebb348f7b526086879b50346bb39f17f1deddd5
Simoneva <33739086+Simoneva@users.noreply.github.com>
2021-09-25 15:02:47 +0800

    Update and rename test1.py to cleanlog.sh
    清理/var/log/文件夹

---
+++ cleanlog.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+rm /var/log/ alternatives.log.* -f
+rm /var/log/ auth.log.* -f
+rm /var/log/ debug.* -f
+rm /var/log/ daemon.log.* -f
+rm /var/log/ dpkg.log.* -f
+rm /var/log/ kern.log.* -f
+rm /var/log/ messages.* -f
+rm /var/log/ syslog.* -f
+rm /var/log/ user.log.* -f
+for i in `find /var/log -name "*.log"`; do cat /dev/null >$i; done
--- test1.py
+++
@@ -1,1 +0,0 @@
-print(r'test\\\n\\')
