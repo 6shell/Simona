f2fc4f6ca36d1736feb079bc5360a1cb5301d318
Simoneva <33739086+Simoneva@users.noreply.github.com>
2021-09-27 10:24:31 +0800

    Create cleanlog-all.sh

---
+++ cleanlog-all.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+ls | egrep 'auth.log..*|debug..*|daemon.log..*|dpkg.log..*|kern.log..*|messages..*|syslog..*|user.log..*' | xargs rm -rf
+for i in `find /var/log -name "*.log"`; do cat /dev/null >$i; done
+for i in `find /var/log -name "*tmp"`; do cat /dev/null >$i; done
