e3a2687d1ec8acedf61a3c722cdacbc860fe9ef4
6shell <33739086+6shell@users.noreply.github.com>
2022-01-17 23:50:38 +0800

    Update cleanlog-all.sh

--- cleanlog-all.sh
+++ cleanlog-all.sh
@@ -2,3 +2,4 @@
 ls | egrep 'auth.log..*|debug..*|daemon.log..*|dpkg.log..*|kern.log..*|messages..*|syslog..*|user.log..*' | xargs rm -rf
 for i in `find /var/log -name "*.log"`; do cat /dev/null >$i; done
 for i in `find /var/log -name "*tmp"`; do cat /dev/null >$i; done
+for i in `find /var/lib/docker/containers/ -name "*-json.log"`; do cat /dev/null >$i; done
