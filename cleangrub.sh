d2caa3265e2cbcb401123b71c7e5d6f2d1b6d81c
6shell <33739086+6shell@users.noreply.github.com>
2023-04-30 15:21:44 +0800

    Create cleangrub.sh

---
+++ cleangrub.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+CurCore="linux-image-`uname -r`"
+CurCoreExtra="linux-image-extra-`uname -r`"
+echo "清理无用的内核"
+echo "当前内核是：$CurCore"
+for i in `dpkg --get-selections|grep linux-image`
+do
+if [ "$i" != "install" ] && [ "$i" != "$CurCore" ]  && [ "$i" != "$CurCoreExtra" ] && [ "$i" != 'linux-image-generic' ];then
+echo "删除无用的内核：$i"
+sudo apt-get remove --purge $i
+fi
+done
+echo "更新启动菜单"
+sudo update-grub
+sudo apt-get autoremove
+sudo apt-get autoclean
